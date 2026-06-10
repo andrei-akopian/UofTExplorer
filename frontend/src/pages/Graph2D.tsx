8e4df829988929fbea4e40016fb32855fa283ce8
andrei-akopian <andrei.a.akopian@gmail.com>
2026-06-10 10:22:55 -0700

    feat: settings component

--- frontend/src/pages/Graph2D.tsx
+++ frontend/src/pages/Graph2D.tsx
@@ -5,6 +5,7 @@ import GraphVis2D from "../components/graph/GraphVis2D";
 import GraphStatsPanel from "../components/graph/GraphStatsPanel";
 import GraphInfoPanel from "../components/graph/GraphInfoPanel";
 import MobileWarning from "../components/MobileWarning";
+import Settings from "../components/graph/Settings";
 import { useSearchParams } from "react-router-dom";
 
 export default function Graph2D() {
@@ -21,9 +22,9 @@ export default function Graph2D() {
     code: "",
     name: "",
   });
-  const [useShellLayout, setUseShellLayout] = useState(true);
   const [loading, setLoading] = useState(false);
   const [manualFetch, setManualFetch] = useState<string>("");
+  const settings: React.ReactNode[] = [];
 
   const [graphData, setGraphData] = useState<GraphData>({
     nodes: [],
@@ -60,21 +61,7 @@ export default function Graph2D() {
           />
         </div>
 
-        <details className="close-on-outclick absolute right-0 z-2 mt-3 mr-2 h-[4.7rem] w-[4.7rem] shrink-0 self-start bg-transparent">
-          <summary className="m-0 flex h-full w-full list-none items-center justify-center p-0 [&::-webkit-details-marker]:hidden">
-            <img src="/settings_gear.svg"></img>
-          </summary>
-          <div className="border-border-dropdown bg-panel-bg text-text-body shadow-dropdown absolute top-[calc(100%+6px)] right-0 left-auto z-1200 flex max-h-[20em] w-[20em] flex-col gap-1.5 overflow-y-auto rounded-md border p-2.5">
-            <label className="flex max-h-none w-max min-w-[5em] items-center gap-2 overflow-visible font-sans text-sm">
-              <input
-                type="checkbox"
-                checked={useShellLayout}
-                onChange={(e) => setUseShellLayout(e.target.checked)}
-              />
-              <span>Use shell layout</span>
-            </label>
-          </div>
-        </details>
+        <Settings settings={settings}></Settings>
       </div>
 
       <div className="fixed bottom-10 left-5 z-20 flex min-w-[20rem] flex-col gap-1">
