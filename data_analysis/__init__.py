32a13e8fe60a095005471c42eb0f6bd300732d78
andrei-akopian <andrei.a.akopian@gmail.com>
2026-06-16 12:43:15 -0700

    fix: outdatedness report versioning

--- data_analysis/__init__.py
+++ data_analysis/__init__.py
@@ -28,7 +28,11 @@ def stats_outdatedness(save=True):
         if not os.path.exists(p):
             print(f"{p} does not exist")
         else:
-            mts = os.path.getmtime(p)
+            if os.path.isdir(p):
+                fp = f"{p}/{os.listdir(p)[0]}"
+            else:
+                fp = p
+            mts = os.path.getmtime(fp)
             mtime = datetime.fromtimestamp(mts)
             delta = today - mtime
             print(
